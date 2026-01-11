ca35701549f7cdf443b789eccc6b676da9f198c6
Miguel Ángel González Santamarta <mgons@unileon.es>
2026-01-11 12:13:49 +0100

    Fixing Copyright year

--- poirot_tui/include/poirot_tui/tui_renderer.hpp
+++ poirot_tui/include/poirot_tui/tui_renderer.hpp
@@ -1,4 +1,4 @@
-// Copyright 2025 Miguel Ángel González Santamarta
+// Copyright 2026 Miguel Ángel González Santamarta
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
